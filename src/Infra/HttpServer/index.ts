5d775e5cf66d17aa93e14b45f2d8a593b897c9c1
ibetech55 <ibetech55@gmail.com>
2026-09-05 22:08:46 -0300

    test 443 in azzure

--- src/Infra/HttpServer/index.ts
+++ src/Infra/HttpServer/index.ts
@@ -34,7 +34,7 @@ class HttpServer {
   // }
 
   listen() {
-    this.app.listen(5010, () => console.log("Listening to 5010"));
+    this.app.listen(443, () => console.log("Listening to 443"));
   }
 
   middlewares() {
